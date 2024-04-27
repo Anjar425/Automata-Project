a55ddcb547228afbaddb9976c28709b3d1799f41
Anjar425 <ekoginanjar4@gmail.com>
2024-04-27 16:03:06 +0700

    fix

--- app.py
+++ app.py
@@ -9,30 +9,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/convert', methods=['POST'])
-# def convert():
-#     regex = request.form.get('regex')  # Ambil nilai 'regex' dari formulir
+@app.route('/convert', methods=['POST'])
+def convert():
+    regex = request.form.get('regex')  # Ambil nilai 'regex' dari formulir
 
-#     logging.debug(f"Regular expression: {regex}")
-#     nfa = convertToNFA(regex)
-#     logging.debug(f"NFA: {nfa}")
+    logging.debug(f"Regular expression: {regex}")
+    nfa = convertToNFA(regex)
+    logging.debug(f"NFA: {nfa}")
 
-#     # Visualisasi NFA
-#     dot = graphviz.Digraph(comment='NFA Visualization')
-#     dot.graph_attr['rankdir'] = 'LR'
+    # Visualisasi NFA
+    dot = graphviz.Digraph(comment='NFA Visualization')
+    dot.graph_attr['rankdir'] = 'LR'
 
-#     # Buat node dan edge sesuai dengan NFA
-#     for state in nfa['states']:
-#         dot.node(state)
-#     for arc in nfa['transition_matrix']:
-#         start, label, end = arc
-#         dot.edge(start, end, label)
+    # Buat node dan edge sesuai dengan NFA
+    for state in nfa['states']:
+        dot.node(state)
+    for arc in nfa['transition_matrix']:
+        start, label, end = arc
+        dot.edge(start, end, label)
 
-#     # Simpan gambar ke file
-#     dot.render('static/nfa_graph', format='svg', cleanup=False)
+    # Simpan gambar ke file
+    dot.render('static/nfa_graph', format='svg', cleanup=False)
 
-#     nfa_generated = True
-#     return render_template('index.html', nfa_image='static/nfa_graph.svg', nfa_generated=nfa_generated )
+    nfa_generated = True
+    return render_template('index.html', nfa_image='static/nfa_graph.svg', nfa_generated=nfa_generated )
 
 
 if __name__ == '__main__':
